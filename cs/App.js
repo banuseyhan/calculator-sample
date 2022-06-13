0266317218651bd9ee59f7f84f2204f69ede8a4b
banuseyhan <95204399+banuseyhan@users.noreply.github.com>
2022-06-13 15:48:27 +0300

    theme dark/light done

--- cs/App.js
+++ cs/App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { useState } from 'react';
+import { StyleSheet, Text, View, Switch } from 'react-native';
+import { ThemeContext } from './src/context/ThemeContext';
 export default function App() {
+  const [theme, setTheme]=useState ('light');
   return (
-    <View style={styles.container}>
+    <View style={theme==='light' ? styles.container:[styles.container, {backgroundColor:'#000'}]}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Switch 
+      value={theme==='light'}
+      onValueChange={() => setTheme(theme==='light'? 'dark': 'light')}/>
     </View>
   );
 }
